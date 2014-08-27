6a22b5094cd1103df3c0ff615c64d8dff7255553
RuinsHe <lyhypacm@gmail.com>
2014-08-27 11:23:55 +0800

    make whole project only use C compiler.

--- tests/compiler_java_test.c
+++ tests/compiler_java_test.c
@@ -1,19 +1,7 @@
 #include "compilers/compiler_java.h"
-#ifdef HAVE_STDIO_H
 #include <stdio.h>
-#else
-#include <cstdio>
-#endif // HAVE_STDIO_H
-#ifdef HAVE_C_ASSERT
 #include <assert.h>
-#else
-#include <cassert>
-#endif // HAVE_C_ASSERT
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#else
-#include <cstdlib>
-#endif // HAVE_STDLIB_H
 
 int main(int argc, char **argv) {
   assert(argc == 3);
